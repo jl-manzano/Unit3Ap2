173efbb7bb42125f066949848a578d336e0bebda
jl-manzano <jl.manzano@iesnervion.es>
2024-12-13 14:37:49 +0100

    ej 6 & 7

--- Unit3Ap2/src/paquetestrings/Ejer6.java
+++ Unit3Ap2/src/paquetestrings/Ejer6.java
@@ -9,13 +9,46 @@ public class Ejer6 {
 
 		// cadena para almacenar la frase introducida por usuario
 		String fraseUsuario = "";
-		
+
 		// pedir frase a usuario
 		System.out.println("Introduzca una frase: ");
-		fraseUsuario = sc.next();
+		fraseUsuario = sc.nextLine();
 
-		
-		
-		
+		if (esPalindroma(fraseUsuario)) {
+			System.out.println("\nEs palíndroma.");
+
+		} else {
+			System.out.println("\nNo es palíndroma.");
+		}
+
+		// cerrar scanner
+		sc.close();
+
 	}
+
+	static boolean esPalindroma(String frase) {
+
+		// variable -> almacenar booleano
+		boolean esPalindroma = false;
+
+		// eliminar espacios frase
+		String fraseSinEspacios = frase.replace(" ", "").toLowerCase();
+
+		// String para almacenar la frase del revés
+		String fraseReves = "";
+
+		// bucle for -> dar vuelta a la frase
+		for (int i = fraseSinEspacios.length() - 1; i >= 0; i--) {
+			fraseReves += fraseSinEspacios.charAt(i);
+		}
+
+		// condicional if -> comprobar si frase es palindroma
+		if (fraseSinEspacios.equals(fraseReves)) {
+			esPalindroma = true;
+		}
+
+		// devolver booleano
+		return esPalindroma;
+	}
+
 }
---
+++ Unit3Ap2/src/paquetestrings/Ejer7.java
@@ -0,0 +1,60 @@
+package paquetestrings;
+
+import java.util.Scanner;
+
+public class Ejer7 {
+	public static void main(String[] args) {
+		// crear scanner
+		Scanner sc = new Scanner(System.in);
+
+		// cadena para almacenar la frase introducida por usuario
+		String fraseUsuario = "";
+		
+		// cadena para almacenar palabra
+		String palabra = "";
+
+		do {
+		// pedir frase a usuario
+		System.out.println("\nIntroduzca una frase: ");
+		fraseUsuario = sc.nextLine();
+		
+		// pedir frase a usuario
+		System.out.println("\nIntroduzca una palabra a buscar en la frase: ");
+		palabra = sc.nextLine();
+		
+		} while (fraseUsuario.isEmpty() || palabra.isEmpty());
+		
+		System.out.println("\nLa palabra " + palabra + " se repite " + encFrase(fraseUsuario, palabra) + " veces en la frase.");
+	
+		// cerrar scanner
+		sc.close();
+	
+	}
+	
+	static int encFrase(String frase, String palabra) {
+		
+		// variable -> almacenar contador
+		int cont = 0;
+		
+		// variable para almacenar el indice
+		int index = 0;
+		
+		// condicional if -> comprobar si usuario no ha escrito nada
+		if (frase.isEmpty() && palabra.isEmpty()) {
+			index = -1;
+		}
+		
+		// bucle while -> determinar cuantas veces se repite la palabra en frase
+		while (index != -1) {
+			
+			// incrementar en 1 el contador
+			cont++;
+			
+			// usar metodo indexOf -> buscar palabra en frase; si encuentra; empieza a buscar desde indice después de palabra encontrada
+			index = frase.indexOf(palabra, index + palabra.length());
+		}
+		
+		// devolver contador
+		return cont;
+	}
+}
